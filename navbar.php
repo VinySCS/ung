d186de9199998dfd47f4d4bb9602afcb4465593b
Viny S.C.S <55089215+VinySCS@users.noreply.github.com>
2020-03-12 21:23:50 -0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg" style="background-color: rgba(0, 255, 13, 0.507); height: 50px;">
+    <div class="collapse navbar-collapse" id="DivMenu">
+        <div class="navbar navbar-brand">
+            <img src="img/logo.png" style="height: 50px; width: 100px;">
+            
+            
+            <a href="index.php" class="navbar-brand"><i class="fa fa-home">Index</i></a>
+            <a href="login.php" class="navbar-brand"><i class="fa fa-user">User</i></a>
+            <a href="contato.php" class="navbar-brand"><i class="fa fa-comments">Contact</i></a>
+            
+            <div class="navbar-nav col-md-10">
+            </div>
+
+            </div>
+    </div>
+</nav>
---
+++ rodape.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-expand-lg" style="background-color: rgba(0, 255, 13, 0.507); height: 45px;">
+            <a href="https://www.facebook.com/viny.souzac.dasilva" target="_blank"><i class="fa fa-facebook-official fa-lg"> </i></a>
+            <a href="https://api.whatsapp.com/send?phone=5511951442948" target="_blank"><i class="fa fa-whatsapp fa-lg"> </i></a>
+            <a href="https://www.instagram.com/viniciusposthuman/" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
+
+            <marquee>Designed by: Viny S.C.S / Web Designer / Programmer Beginner / Database Beginner </marquee>
